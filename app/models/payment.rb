a393d7a1b87fc364797256f23712509133a226e1
gregspagnolo <dwdrummer@gmail.com>
2016-02-10 22:47:26 -0600

    Fix active card bug

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -13,8 +13,7 @@ class Payment < ActiveRecord::Base
 	def process_payment
       customer = Stripe::Customer.create email: email, card: token
 
-      Stripe::Charge.create customer: customer.id,
-                             amount: 1000,
+      Stripe::Charge.create  amount: 1000,
                              description: 'Premium',
                              currency: 'usd'
 	end
